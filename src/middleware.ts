acbe0988d352e9de287cb8c27b8ce8dbe880d705
hamza-med <hamza.mohamed1998@gmail.com>
2024-06-21 17:45:23 +0100

    add all dependancies and the authentication first steps

---
+++ src/middleware.ts
@@ -0,0 +1,10 @@
+import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware({
+  publicRoutes: ["/", "/auth(.*)", "/portal(.*)","/images(.*)"],
+  ignoredRoutes: ["/chatbot"],
+});
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
